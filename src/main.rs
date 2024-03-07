934d09fef6a5226ee5dd38fa0170c8533cc8e181
Gabor Benko <benkogabor87@gmail.com>
2024-03-07 16:22:22 +0100

    read args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 
 fn main() {
-    let arguments:Vec<String> = env::args().collect();
-    dbg!(arguments);
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
