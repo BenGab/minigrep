eea540c479e77cefb594d1105f5d02cd6bafb33b
Gabor Benko <benkogabor87@gmail.com>
2024-03-07 19:07:52 +0100

    using result

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +11,17 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Sould have been able to read file");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\r\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
